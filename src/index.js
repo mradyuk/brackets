835f0f19518efdd1045aeaafad8689d7a781b2b9
mradyuk <mariaradyuk@gmail.com>
2019-03-11 11:57:11 +0300

    add logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   const opening = [];
+  const closing = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    const arr = bracketsConfig[i];
+    opening.push(arr[0]);
+    closing.push(arr[1]);
+  }
+  
+  let stack = [];
+
+  if (str.length === 0) {
+    return true;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (closing.indexOf(char) === opening.indexOf(char)) {
+      if (stack.length !== 0 && stack[stack.length - 1] !== char) {
+        stack.push(char);
+      }
+      if (stack.length !== 0 && stack[stack.length - 1] === char) {
+        stack.pop(char);
+      }
+    } else {
+      if (opening.indexOf(char) > -1) {
+        stack.push(char);
+      } else if (closing.indexOf(char) > -1) {
+        const pair = opening[closing.indexOf(char)];
+        if (stack.pop() !== pair) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return stack.length == 0;
 }
